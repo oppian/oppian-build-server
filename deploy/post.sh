b7a367a2ec7000c849a30d4ec06950708eb8171e
oppianmatt <matt@oppian.com>
2010-03-10 19:38:54 +0000

    start daemons in post script

--- deploy/post.sh
+++ deploy/post.sh
@@ -2,6 +2,9 @@
 
 # This is called post download and used to setup the environment for the first time.
 
+# argument will be deploy dir
+DEPLOY_DIR=$1
+
 # create the virtual env
 python boot.py build-env
 
@@ -14,3 +17,15 @@ pip install -r requirements.txt
 # copy ssh key and set mode
 cp deploy/id_rsa ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
+
+# start the daemons
+for botdir in master-oserver slave-localhost ; do
+  echo $botdir
+  cd $botdir
+  if [ -e $botdir/twistd.pid ] ; then
+    # stop if needed
+    buildbot stop
+  fi
+  buildbot start
+  cd ..
+done
