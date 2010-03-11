9caa9c916bc1d9e7c4427af9e6810e2965956729
oppianmatt <matt@oppian.com>
2010-03-11 02:43:39 +0000

    supports building/deploying the oserver

--- deploy/post.sh
+++ deploy/post.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 # This is called post download and used to setup the environment for the first time.
 
 # argument will be deploy dir
 DEPLOY_DIR=$1
+HOSTNAME=build.oppian.com
 
 # kill off any other buildbots
-pkill -9 -f buildbot
+(pgrep -f buildbot)
+(pkill -9 -f buildbot)
 
 # create the virtual env
 python boot.py build-env
@@ -25,3 +27,20 @@ chmod 600 ~/.ssh/id_rsa
 for botdir in master-oserver slave-localhost ; do
   buildbot start $botdir
 done
+
+# apache
+
+# enable mod_proxy
+a2enmod proxy
+a2enmod proxy_http
+
+# rewrite template
+sed -e "s|@DEPLOY_DIR@|$DEPLOY_DIR|g" -e "s|@HOSTNAME@|$HOSTNAME|g" $DEPLOY_DIR/apache/http.conf.template > $DEPLOY_DIR/apache/http.conf
+
+echo "Linking to apache config..."
+ln -s -f $DEPLOY_DIR/apache/http.conf /etc/apache2/sites-available/build
+a2ensite build
+
+echo "Restarting apache..."
+apache2ctl configtest
+apache2ctl restart
